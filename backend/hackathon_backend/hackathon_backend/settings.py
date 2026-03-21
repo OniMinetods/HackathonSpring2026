5a636f936ceea4933992aa632904ba792efd7723
vaTrushkens <oleg.ermakov228@mail.ru>
2026-03-21 14:38:59 +0300

    fix: fixed db_con && requirements

--- backend/hackathon_backend/hackathon_backend/settings.py
+++ backend/hackathon_backend/hackathon_backend/settings.py
@@ -55,9 +55,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hackathon_backend.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "12345",
+        "HOST": "192.168.2.51",
+        "PORT": "5432",
     }
 }
 
